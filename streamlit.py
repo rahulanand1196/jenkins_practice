82e5c6a5d3e295de86d7b71ddf62111928b2c535
rahul <rahulanand1196@gmail.com>
2023-10-12 01:03:30 +0530

    testing

---
+++ streamlit.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def UI():
+    st.set_page_config(
+        page_title="PDF_GENERATOR",
+        page_icon="🤖",
+    )
+    
+    st.write('develop')
+
+
+
+if __name__ == "__main__":
+    UI()
